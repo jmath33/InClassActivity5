5cf7c9fe9a8474a1bd15ad019e08f56eee8ab42a
Jonathan Mather <matherj@flip1.engr.oregonstate.edu>
2021-04-25 23:03:55 -0700

    Leap Year Program, No Error Handling

--- jonathan_mather_hw1.py
+++ jonathan_mather_hw1.py
@@ -1,6 +1,6 @@
 in_year = int(input("Please enter a year to check if it is a leap year: ")
 
-if((in_year % 4) == 0):
+if ((in_year % 4) == 0):
 	if((in_year % 100) == 0):
 		if((in_year % 400) == 0):
 			print("%d is a leap year!" %in_year)
